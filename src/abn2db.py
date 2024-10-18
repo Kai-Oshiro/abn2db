a18cbc6ec0341fcc455e43c59e07d695a9223af2
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2024-10-18 19:57:33 +0900

    update main process

--- src/abn2db.py
+++ src/abn2db.py
@@ -2,15 +2,5 @@
 
 from module.abn import Abn
 
-star_line = "**************************************************"
-dash_line = "--------------------------------------------------"
-eq_line   = "=================================================="
-
-abn = Abn("/LARGE0/gr10563/kai/scripts/abn2db/test/ML_ABN_100-Ce_2L0V")
-all_data = abn.read()
-
-for key, value in all_data["header_data"].items():
-    print(f"{key}: {value}")
-
-for key, value in all_data["configurations"][0].items():
-    print(f"{key}: {value}")
+abn = Abn("/LARGE0/gr10563/kai/scripts/abn2db/test/ML_ABN_100-Ce_2L0V_TEST")
+abn.read_file()
