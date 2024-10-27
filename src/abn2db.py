13cdfea4263af1110615b33593c6e802a6f4187b
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2024-10-27 21:54:50 +0900

    update entire process

--- src/abn2db.py
+++ src/abn2db.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+import os
+
+import ase
 
 from module.abn import Abn
 
-abn = Abn("/LARGE0/gr10563/kai/scripts/abn2db/test/ML_ABN_100-Ce_2L0V_TEST")
-abn.read_file()
+file_path = "/LARGE0/gr10563/kai/scripts/abn2db/test/ML_ABN_100-Ce_2L0V_TEST"
+abn = Abn(file_path)
+#abn.read_file()
+header_data, training_data = abn.read_file()
+
+file_name = os.path.basename(file_path)
