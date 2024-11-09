218a65935e30525fa413257af6afb388eb5325fd
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2024-11-09 15:11:31 +0900

    add read_db method

--- src/module/db.py
+++ src/module/db.py
@@ -3,6 +3,7 @@ import os
 from ase import Atoms
 from ase.calculators.singlepoint import SinglePointDFTCalculator
 from ase.db import connect
+from ase.data import atomic_numbers, atomic_masses
 
 class Database:
     def __init__(self):
@@ -24,6 +25,8 @@ class Database:
 
         db = self._make_db()
 
+        ref_energy = self.header_data["ref_energy"]
+        mass = self.header_data["mass"]
         for result_dict in self.training_data:
             unique_species = list(result_dict["atom_type_num"].keys())
             species = [key for key, count in result_dict["atom_type_num"].items() for _ in range(count)]
@@ -51,6 +54,7 @@ class Database:
             atoms.set_calculator(calculator)
             atoms.calc.name = "vasp"
 
+            sys_name = str(result_dict["sys_name"])
             conf_num = int(result_dict["conf_num"])
             ctifor = float(result_dict["ctifor"])
 
@@ -65,7 +69,136 @@ class Database:
 
             db.write(
                 atoms,
+                sys_name=sys_name,
                 conf_num=conf_num,
                 ctifor=ctifor,
-                data={"basis": basis}
+                data={
+                    "basis": basis,
+                    "ref_energy": ref_energy,
+                    "mass": mass
+                    }
                 )
+
+    def read_db(self, db_path, ref_energy=None, mass=None):
+        self.db_path = db_path
+        self.ref_energy = ref_energy
+        self.mass = mass
+
+        db = connect(self.db_path)
+
+        header_data = {}
+        training_data = []
+
+        count = 1
+        max_n_atom_type = 0
+        all_atom_type = []
+        max_n_atom_per_sys = 0
+        max_n_atom_per_type = 0
+        n_basis = {}
+        all_basis = {}
+        for row in db.select():
+            conf_num = count
+            sys_name = row["sys_name"]
+
+            atoms = row.toatoms()
+            species = atoms.get_chemical_symbols()
+            n_atom_type = len(set(species))
+            n_atom = len(species)
+            #print(f"conf_num: {conf_num}, sys_name: {sys_name}, n_atom_type: {n_atom_type}, n_atom: {n_atom}")
+
+            atom_type_num = {}
+            for element in species:
+                if element in atom_type_num:
+                    atom_type_num[element] += 1
+                else:
+                    atom_type_num[element] = 1
+            #print(f"atom_type_num: {atom_type_num}")
+
+            ctifor = row["ctifor"]
+            basis = row.data["basis"]
+            vectors = row.cell
+            positions = row.positions
+            # The "energy" in the ML_ABN file corresponds to the "free energy" in the OUTCAR file, 
+            # so it is registered as "free energy" in the atoms object.
+            # On the other hand, variables and keys conform to the ML_ABN file 
+            # and are represented as "energy".
+            energy = row.free_energy
+            forces = row.forces
+            stress = row.stress
+
+            training_data.append({
+                "conf_num": conf_num,
+                "ctifor": ctifor,
+                "atom_type_num": atom_type_num,
+                "vectors": vectors,
+                "positions": positions,
+                "energy": energy,
+                "forces": forces,
+                "stress": stress
+                })
+
+            # Get header data
+            if n_atom_type > max_n_atom_type:
+                max_n_atom_type = n_atom_type
+
+            for element in atom_type_num.keys():
+                if element not in all_atom_type:
+                    all_atom_type.append(element)
+
+                # Count basis data
+                if element in n_basis:
+                    n_basis[element] += len(basis[element])
+                else:
+                    n_basis[element] = len(basis[element])
+
+                # Get basis data
+                basis_tag = f"basis_for_{element}"
+                if basis_tag not in all_basis:
+                    all_basis[basis_tag] = {}
+
+                if len(basis[element]) > 0:
+                    all_basis[basis_tag][conf_num] = basis[element]
+
+            if n_atom > max_n_atom_per_sys:
+                max_n_atom_per_sys = n_atom
+
+            if max(atom_type_num.values()) > max_n_atom_per_type:
+                max_n_atom_per_type = max(atom_type_num.values())
+
+            if self.ref_energy is None:
+                self.ref_energy = row.data.get("ref_energy", None)
+            if self.mass is None:
+                self.mass = row.data.get("mass", None)
+
+            count += 1
+
+        # Set header data
+        header_data["n_conf"] = count
+        header_data["max_n_atom_type"] = max_n_atom_type
+        header_data["all_atom_type"] = all_atom_type
+        header_data["max_n_atom_per_sys"] = max_n_atom_per_sys
+        header_data["max_n_atom_per_type"] = max_n_atom_per_type
+
+        if self.ref_energy is None:
+            self.ref_energy = []
+            for _ in range(max_n_atom_type):
+                self.ref_energy.append(0.0)
+        header_data["ref_energy"] = self.ref_energy
+
+        if self.mass is None:
+            self.mass = []
+            for element in all_atom_type:
+                an = atomic_numbers[element]
+                self.mass.append(atomic_masses[an])
+        header_data["mass"] = self.mass
+
+        header_data["n_basis"] = n_basis
+
+        for basis_tag in all_basis.keys():
+            header_data[basis_tag] = all_basis[basis_tag]
+
+        print("### Header data ###")
+        for key, value in header_data.items():
+            print(f"{key}: {value}")
+
+        return header_data, training_data
