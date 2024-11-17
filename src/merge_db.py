4cee728b3f7e55b1b6188e6e0b62c51ac50fc27b
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2024-11-17 16:39:59 +0900

    support multiple db files

--- src/merge_db.py
+++ src/merge_db.py
@@ -6,11 +6,8 @@ from ase.db import connect
 
 parser = argparse.ArgumentParser()
 # Positional arguments
-parser.add_argument("db1_file", type=str,
-                    help="Path to the first ML_ABN file to be loaded")
-
-parser.add_argument("db2_file", type=str,
-                    help="Path to the second ML_ABN file to be loaded")
+parser.add_argument("db_files", type=str, nargs='+',
+                    help="Paths to the ML_ABN files to be loaded")
 
 # Optional arguments
 parser.add_argument("-fn", "--file_name", type=str, default=None,
@@ -18,15 +15,13 @@ parser.add_argument("-fn", "--file_name", type=str, default=None,
 
 # Parse the arguments
 args = parser.parse_args()
-db1_path = args.db1_file
-db2_path = args.db2_file
+db_paths = args.db_files
 
 # Define the path for the new database file
 new_db_name = args.file_name
 if new_db_name is None:
-    db1_name = os.path.basename(db1_path).split(".")[0]
-    db2_name = os.path.basename(db2_path).split(".")[0]
-    new_db_name = db1_name + "-" + db2_name
+    base_names = [os.path.basename(db_path).split(".")[0] for db_path in db_paths]
+    new_db_name = "-".join(base_names)
 new_db_name = new_db_name + ".db"
 
 cwd = os.getcwd()
@@ -36,18 +31,11 @@ new_db_path = os.path.join(cwd, new_db_name)
 if os.path.exists(new_db_path):
     raise FileExistsError(f"{new_db_path} already exists.")
 
-# Connect to the first database
-db1 = connect(db1_path)
-# Connect to the second database
-db2 = connect(db2_path)
 # Connect to the output database
 new_db = connect(new_db_path, use_lock_file=False)
 
-# Copy all rows from the first database to the output database
-for row in db1.select():
-    new_db.write(row.toatoms(), key_value_pairs=row.key_value_pairs, data=row.data)
-
-# Copy all rows from the second database to the output database
-for row in db2.select():
-    new_db.write(row.toatoms(), key_value_pairs=row.key_value_pairs, data=row.data)
-
+# Copy all rows from each database to the output database
+for db_path in db_paths:
+    db = connect(db_path)
+    for row in db.select():
+        new_db.write(row.toatoms(), key_value_pairs=row.key_value_pairs, data=row.data)
