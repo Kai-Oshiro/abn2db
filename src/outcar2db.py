6ae0d6f6d5860b8c3ff15120f41a44785e100788
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2025-01-04 14:01:49 +0900

    add main() function

--- src/outcar2db.py
+++ src/outcar2db.py
@@ -6,20 +6,24 @@ import argparse
 from module.outcar import Outcar
 from module.db import Database
 
-parser = argparse.ArgumentParser()
-# Positional arguments
-parser.add_argument("outcar_file", type=str,
-                    help="Path of OUTCAR file to be loaded")
+def main():
+    parser = argparse.ArgumentParser()
+    # Positional arguments
+    parser.add_argument("outcar_files", type=str, nargs='+',
+                        help="Path of OUTCAR file to be loaded")
 
-# Optional arguments
-parser.add_argument("-fn", "--file_name", type=str, default=None,
-                    help="Name of new ASE database file.")
+    # Optional arguments
+    parser.add_argument("-fn", "--file_name", type=str, default=None,
+                        help="Name of new ASE database file.")
 
-# Parse the arguments
-args = parser.parse_args()
-outcar_path = args.outcar_file
-outcar_path = os.path.abspath(outcar_path)
-#print(f"outcar_path: {outcar_path}")
+    # Parse the arguments
+    args = parser.parse_args()
+    outcar_path = args.outcar_files
+    outcar_path = os.path.abspath(outcar_path)
+    #print(f"outcar_path: {outcar_path}")
 
-oc = Outcar()
-oc.read_outcar(outcar_path)
+    #oc = Outcar()
+    #entire_data = oc.read_outcar(outcar_path)
+
+if __name__ == "__main__":
+    main()
