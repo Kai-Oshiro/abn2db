519fcc89aa14ea8e66c14b4e842b8651e8cff9d0
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2025-01-06 18:56:29 +0900

    support slice-format arguments

--- src/outcar2db.py
+++ src/outcar2db.py
@@ -6,32 +6,25 @@ import argparse
 from module.outcar import Outcar
 from module.db import Database
 
-def parse_range(range_str):
-    """Parse a range string like '1:3' or '-3:-1' into a list of integers."""
-    if ':' in range_str:
-        start, end = map(int, range_str.split(':'))
-        if start > 0:
-            start -= 1
-        if end < 0:
-            end += 1
-        return list(range(start, end))
-    else:
-        index = int(range_str)
-        if index > 0:
-            index += 1
-        return [index]
+def parse_slice(slice_str):
+    """Parse a slice string like '1:3:2' or '-3:' into a slice object."""
+    parts = slice_str.split(':')
+    start = int(parts[0]) if parts[0] else None
+    end = int(parts[1]) if len(parts) > 1 and parts[1] else None
+    step = int(parts[2]) if len(parts) > 2 and parts[2] else None
+    return slice(start, end, step)
 
 def main():
     parser = argparse.ArgumentParser()
     # Positional arguments
-    parser.add_argument("outcar_files", type=str, nargs='+',
+    parser.add_argument("outcar_files", type=str, nargs="+",
                         help="Path of OUTCAR file to be loaded")
 
     # Optional arguments
     parser.add_argument("-fn", "--file_name", type=str, default=None,
                         help="Name of new ASE database file.")
 
-    parser.add_argument("-is", "--ionic_step", type=str, nargs='+', default=None,
+    parser.add_argument("-is", "--ionic_step", type=str, nargs="+", default=None,
                         help="Ionic step you want to get (e.g., '1:3', '1 3 5' or '-1').\n\
                         To specify a range with negative value,\
                         enclose the range in quotation marks and put leading spaces,\
@@ -50,15 +43,24 @@ def main():
     if args.ionic_step:
         indices = []
         for ionic_step in args.ionic_step:
-            indices.extend(parse_range(ionic_step))
+            if ":" in ionic_step:
+                indices.append(parse_slice(ionic_step))
+            else:
+                indices.append(int(ionic_step.strip()))
+
+    oc = Outcar()
 
     all_outcar_data = []
     for outcar_path in outcar_files:
-        oc = Outcar()
         outcar_data = oc.read_outcar(outcar_path)
 
         if indices:
-            data_to_process = [outcar_data[i] for i in indices if i < len(outcar_data)]
+            data_to_process = []
+            for index in indices:
+                if isinstance(index, slice):
+                    data_to_process.extend(outcar_data[index])
+                else:
+                    data_to_process.append(outcar_data[index])
         else:
             data_to_process = outcar_data
 
