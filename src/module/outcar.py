f2230c3f9df13661706b89e60c6b545f35c3cb9f
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2025-01-07 20:07:10 +0900

    update parse_data function

--- src/module/outcar.py
+++ src/module/outcar.py
@@ -39,6 +39,12 @@ class Outcar:
             if self.n_atom_per_type_tag in line:
                 n_atom_per_type = list(map(int, line.split("=")[1].split()))
 
+                if len(atom_type) != len(n_atom_per_type):
+                    raise ValueError("Number of atom types and number of atoms per type do not match.")
+                else:
+                    n_atom_type = len(atom_type)
+                    atom_type_num = dict(zip(atom_type, n_atom_per_type))
+
             if self.system_tag in line:
                 sys_name = line.split()[-1]
 
@@ -54,9 +60,9 @@ class Outcar:
                     ionic_step = int(line.split()[-2])
                     step_results = {"file_name": file_name}
                     step_results["ionic_step"] = ionic_step
-                    step_results["atom_type"] = atom_type
                     step_results["n_atom"] = n_atom
-                    step_results["n_atom_per_type"] = n_atom_per_type
+                    step_results["n_atom_type"] = n_atom_type
+                    step_results["atom_type_num"] = atom_type_num
                     step_results["sys_name"] = sys_name
                     step_results["mass"] = mass
 
@@ -97,24 +103,60 @@ class Outcar:
         header_data = {}
         training_data = []
 
-        ### ここをアップデートする ###
-        # Extract header data
-        header_data["atom_type"] = outcar_data[0]["atom_type"]
-        header_data["n_atom"] = outcar_data[0]["n_atom"]
-        header_data["n_atom_per_type"] = outcar_data[0]["n_atom_per_type"]
-        header_data["sys_name"] = outcar_data[0]["sys_name"]
-        header_data["mass"] = outcar_data[0]["mass"]
+        count = 1
+        max_n_atom_type = 0
+        all_atom_type = []
+        max_n_atom_per_sys = 0
+        max_n_atom_per_type = 0
+        ref_energy = []
+        mass = []
+        n_basis = []
+        for data in outcar_data:
+            # Extract header data
+            if data["n_atom_type"] > max_n_atom_type:
+                max_n_atom_type = data["n_atom_type"]
 
-        # Extract training data
-        for step in outcar_data:
-            training_data.append({
-            "ionic_step": step["ionic_step"],
-            "vectors": step["vectors"],
-            "positions": step["positions"],
-            "forces": step["forces"],
-            "energy": step["energy"],
-            "stress": step["stress"]
-            })
+            for index, (atom_type, n_atom_per_type) in enumerate(data["atom_type_num"].items()):
+                if atom_type not in all_atom_type:
+                    all_atom_type.append(atom_type)
+
+                    ref_energy.append(0.0)
+
+                if n_atom_per_type > max_n_atom_per_type:
+                    max_n_atom_per_type = n_atom_per_type
+
+                if data["mass"][index] not in mass:
+                    mass.append(data["mass"][index])
+
+            # Add n_basis related process
+
+            if data["n_atom"] > max_n_atom_per_sys:
+                max_n_atom_per_sys = data["n_atom"]
+
+            header_data["max_n_atom_type"] = max_n_atom_type
+            header_data["all_atom_type"] = all_atom_type
+            header_data["max_n_atom_per_sys"] = max_n_atom_per_sys
+            header_data["max_n_atom_per_type"] = max_n_atom_per_type
+            header_data["ref_energy"] = ref_energy
+            header_data["mass"] = mass
+            header_data["n_basis"] = n_basis
+
+            # Extract training data
+            result_dict = {"conf_num": count}
+            result_dict["sys_name"] = data["sys_name"]
+            result_dict["n_atom_type"] = data["n_atom_type"]
+            result_dict["n_atom"] = data["n_atom"]
+            result_dict["atom_type_num"] = data["atom_type_num"]
+            result_dict["ctifor"] = 0.002
+            result_dict["vectors"] = data["vectors"]
+            result_dict["positions"] = data["positions"]
+            result_dict["energy"] = data["energy"]
+            result_dict["forces"] = data["forces"]
+            result_dict["stress"] = data["stress"]
+
+            training_data.append(result_dict)
+
+            count += 1
 
         return header_data, training_data
 
