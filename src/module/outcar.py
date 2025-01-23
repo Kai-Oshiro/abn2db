21ab969aa7d6e3a9c6785f915fa0f3ba8a429f55
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2025-01-23 18:54:11 +0900

    update load method

--- src/module/outcar.py
+++ src/module/outcar.py
@@ -3,17 +3,25 @@ import re
 
 class Outcar:
     def __init__(self):
+        # Keywords for common parameters
         self.atom_type_tag = "VRHFIN"
+        self.mass_tag = "mass and valenz"
         self.n_atom_tag = "NIONS"
         self.n_atom_per_type_tag = "ions per type ="
         self.system_tag = "POSCAR ="
-        self.mass_tag = "mass and valenz"
+
+        # Keywords for DFT and MLFF sections
+        self.dft_section_flag = "FORCE on cell =-STRESS in cart. coord.  units (eV):"
+        self.mlff_section_flag = "ML FORCE on cell =-STRESS in cart. coord. units (eV/cell)"
+
+        # Keywords for DFT and MLFF results
         self.ionic_step_tag = "Ionic step"
+        self.stress_tag = "in kB"
         self.vectors_tag = "direct lattice vectors"
         self.position_tag = "POSITION"
         self.force_tag = "TOTAL-FORCE (eV/Angst)"
-        self.energy_tag = "free  energy   TOTEN  ="
-        self.stress_tag = "in kB"
+        self.free_energy_tag = "free  energy" # Handle both MLFF and DFT energies
+        self.energy_tag = "energy  without entropy="
 
     def load(self, outcar_path):
         """
@@ -54,14 +62,18 @@ class Outcar:
         with open(outcar_path, 'r') as f:
             lines = f.readlines()
 
-        self.start_ionic_step = False
-        outcar_data = []
+        ### Get common parameters ###
         element_list = []
         mass = []
         for idx, line in enumerate(lines):
             if self.atom_type_tag in line:
                 match = re.search(r"=(.*?):", line)
                 element_list.append(match.group(1).strip())
+
+            if self.mass_tag in line:
+                mass_line = lines[idx].split(";")[0]
+                mass_line = mass_line.replace("POMASS =", "").strip()
+                mass.append(float(mass_line))
 
             if self.n_atom_tag in line:
                 n_atom = int(line.split()[-1])
@@ -71,56 +83,82 @@ class Outcar:
 
                 if len(element_list) != len(n_atom_per_type):
                     raise ValueError("Number of atom types and number of atoms per type do not match.")
-                else:
-                    n_atom_type = len(element_list)
-                    atom_type_num = dict(zip(element_list, n_atom_per_type))
+
+                n_atom_type = len(element_list)
+                atom_type_num = dict(zip(element_list, n_atom_per_type))
 
             if self.system_tag in line:
                 sys_name = line.replace(self.system_tag, "").strip()
 
-            if self.mass_tag in line:
-                mass_line = lines[idx].split(";")[0]
-                mass_line = mass_line.replace("POMASS =", "").strip()
-                mass.append(float(mass_line))
+                break
 
+        # Add common parameters to the dictionary
+        common_data = {"file_name": file_name}
+        common_data["mass"] = mass
+        common_data["n_atom"] = n_atom
+        common_data["n_atom_type"] = n_atom_type
+        common_data["atom_type_num"] = atom_type_num
+        common_data["sys_name"] = sys_name
+
+        ### Get iteration results ###
+        outcar_data = []
+        is_dft = False
+        is_step = False
+        for idx, line in enumerate(lines):
             if self.ionic_step_tag in line:
-                self.start_ionic_step = True
+                ionic_step = int(line.split()[-2])
 
-            if self.start_ionic_step:
-                if self.ionic_step_tag in line:
-                    ionic_step = int(line.split()[-2])
-                    step_results = {"file_name": file_name}
-                    step_results["ionic_step"] = ionic_step
-                    step_results["n_atom"] = n_atom
-                    step_results["n_atom_type"] = n_atom_type
-                    step_results["atom_type_num"] = atom_type_num
-                    step_results["sys_name"] = sys_name
-                    step_results["mass"] = mass
+                is_step = True
 
-                if self.vectors_tag in line:
-                    vectors = []
-                    for i in range(3):
-                        vectors.append([float(x) for x in lines[idx+i+1].split()[0:3]])
-                    step_results["vectors"] = vectors
+            # Skip the header section of OUTCAR
+            if not is_step:
+                continue
 
-                if self.position_tag in line:
-                    positions = []
-                    forces = []
-                    for i in range(n_atom):
-                        positions.append([float(x) for x in lines[idx+i+2].split()[0:3]])
-                        forces.append([float(x) for x in lines[idx+i+2].split()[3:6]])
-                    step_results["positions"] = positions
-                    step_results["forces"] = forces
+            if self.dft_section_flag in line:
+                is_dft = True
 
-                if self.energy_tag in line:
+            if self.mlff_section_flag in line:
+                is_dft = False
+
+            # Get DFT and MLFF results
+            if self.stress_tag in line:
+                stress = [float(x) for x in lines[idx].split()[2:8]]
+
+            if self.vectors_tag in line:
+                vectors = []
+                for i in range(3):
+                    vectors.append([float(x) for x in lines[idx+i+1].split()[0:3]])
+
+            if self.position_tag in line:
+                positions = []
+                forces = []
+                for i in range(n_atom):
+                    positions.append([float(x) for x in lines[idx+i+2].split()[0:3]])
+                    forces.append([float(x) for x in lines[idx+i+2].split()[3:6]])
+
+            if self.free_energy_tag in line:
+                if is_dft:
+                    free_energy = float(line.split()[4])
+                else:
+                    free_energy = float(line.split()[5])
+
+            if self.energy_tag in line:
+                if is_dft:
+                    energy = float(line.split()[3])
+                else:
                     energy = float(line.split()[4])
-                    step_results["energy"] = energy
 
-                if self.stress_tag in line:
-                    stress = [float(x) for x in lines[idx].split()[2:8]]
-                    step_results["stress"] = stress
+                step_data = {"ionic_step": ionic_step}
+                step_data.update(common_data)
+                step_data["is_dft"] = is_dft
+                step_data["stress"] = stress
+                step_data["vectors"] = vectors
+                step_data["positions"] = positions
+                step_data["forces"] = forces
+                step_data["free_energy"] = free_energy
+                step_data["energy"] = energy
 
-                    outcar_data.append(step_results)
+                outcar_data.append(step_data)
 
         return outcar_data
 
